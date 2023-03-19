07d95f13e8fb0d0df9f7d972d52373e1c74e2451
NicoMori <59441667+Nico-Mori@users.noreply.github.com>
2023-03-19 17:34:07 +0100

    fixes and upgrades
    top n purchases detail, fix on split calculation

--- source_codes/ExpenseTracking/expenses_report.py
+++ source_codes/ExpenseTracking/expenses_report.py
@@ -24,7 +24,9 @@ for i in filepaths:
 
     #Unified criteria for ammount column
     temp['Costo'] = pd.to_numeric(temp['Costo'])
-    temp['Costo']=np.where(temp[my_name]==0, temp['Costo'], temp['Costo']/(len(temp.loc[:,'Valuta':].columns)-1))
+    temp['Costo']=np.select([temp[my_name]>0, abs(temp.iloc[:,5:]).sum(axis=1)==0, temp[my_name]<0],
+                            [temp['Costo']+temp.iloc[:,5:].drop(columns=[my_name]).sum(axis=1), temp['Costo'], abs(temp[my_name])], 
+                            default=0)
 
     #Keep relevant columns
     temp = temp.loc[:,:'Costo']
@@ -34,8 +36,11 @@ for i in filepaths:
 
 #Reformat df
 df['Costo'] = df['Costo'].round(2)
-df['Data'] = pd.to_datetime(df['Data'])
-df = df[['Costo','Data','Descrizione','Categorie']]
+df['Data'] = pd.to_datetime(df['Data']).dt.to_period('M') #We are interested only on the month
+df = df[['Costo','Data','Descrizione','Categorie']].sort_values(['Data'])
+
+#Keep data for the last 12 months
+df = df[df['Data']>=(datetime.now().date() - pd.DateOffset(months=12)).to_period('M')]
 
 
 ### Add custom categories ###
@@ -47,8 +52,26 @@ df_cat = df_cat.drop(['Categorie','Gruppi'], axis=1)
 
 
 ### Summarise data ###
+#Top spenders by month
+df_topn =(df_cat[(df_cat.Categoria!='Mangiare') & (~df_cat.Descrizione.isin(['Affitto','Crossfit']))]
+    .sort_values(by=['Data','Costo'], ascending=[False,False])
+    .groupby('Data')
+    .head(5)
+    .drop(columns=['Categoria'])
+    )
+
+today=str(datetime.now().date())
+
+with open(f'top_spenders_{today}.txt', 'w') as f:
+    
+    for i in df_topn['Data'].unique():
+        f.write(f'Top purchases {i}:\n')
+        f.write(df_topn[df_topn['Data']==i].drop(columns=['Data']).to_string(index=False, header=False))
+        f.write('\n\n')
+
+
 #Group expenses by category and month
-df_summ = df_cat.groupby([df_cat.Data.dt.month, df_cat.Categoria])['Costo'].sum()
+df_summ = df_cat.groupby([df_cat.Data, df_cat.Categoria])['Costo'].sum()
 df_summ = df_summ.unstack('Categoria')
 
 df_summ = df_summ.fillna(0)
